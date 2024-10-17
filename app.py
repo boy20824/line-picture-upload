eced280bdd9b55f8454d6a998a8e0c45e94d6729
john <boy20824@gmail.com>
2024-10-18 02:34:00 +0800

    Remove sensitive information

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from linebot.models import MessageEvent, ImageMessage, TextSendMessage
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+import json
 import requests
 import os
 
